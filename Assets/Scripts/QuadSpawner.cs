21155a89ebcad26d5dc01124cdd40c5f50521fad
prupkey <prupkey@vt.edu>
2021-10-17 20:19:47 -0400

    scipt work

--- Assets/Scripts/QuadSpawner.cs
+++ Assets/Scripts/QuadSpawner.cs
@@ -10,38 +10,77 @@ public class QuadSpawner : MonoBehaviour
     Vector3 pointA = new Vector3(0, 0);
     Vector3 pointB = new Vector3(0, 0);
     Vector3 pointC = new Vector3(0, 0);    // set 0s
-    bool solidMesh = false; // set false
+    // bool solidMesh = false; // set false
+    public GameObject spawnTest;
+
+
+    Vector3[] verticies = new Vector3[3];
+    Vector2[] uv = new Vector2[3];
+    int[] triangles = new int[3];
+    Mesh[] quads = new Mesh[] { }; // array of quads to allow player to spawn more than one
 
     // OVRInput.Controller controller = OVRInput.Controller.RTouch;
 
-    void getpostitions()
+    void createQuad()
     {
         if(OVRInput.GetUp(OVRInput.Button.Two))
         {
             if (stage == 0)
             {
-                Vector3 pointA = conRight.transform.position;
+                pointA = conRight.transform.position;
                 stage = stage + 1;
                 Debug.LogError("stage 1 - point stored");
             }
             else if (stage == 1)
             {
-                Vector3 pointB = conRight.transform.position;
+                pointB = conRight.transform.position;
                 stage = stage + 1;
                 Debug.LogError("stage 2 - point stored");
             }
             else if (stage == 2)
             {
-                Vector3 pointC = conRight.transform.position;
-                stage = 0;
-                solidMesh = true;
+                pointC = conRight.transform.position;
+                
                 Debug.LogError("stage 3 - point stored");
+
+                Mesh mesh = new Mesh();
+
+                verticies[0] = pointA;
+                verticies[1] = pointB;
+                verticies[2] = pointC;
+
+                uv[0] = new Vector2(0, 0);
+                uv[1] = new Vector2(0, 1);
+                uv[2] = new Vector2(1, 1);
+
+                triangles[0] = 0;
+                triangles[1] = 1;
+                triangles[2] = 2;
+
+                mesh.vertices = verticies;
+                mesh.uv = uv;
+                mesh.triangles = triangles;
+
+                GetComponent<MeshFilter>().mesh = mesh;
+
+
+
+
+
+                Debug.LogError("SPAWNED");
+                stage = 0;
+
+
+
+                spawnObjectTest(pointA);
+                spawnObjectTest(pointB);
+                spawnObjectTest(pointC);
+
 
             }
             else
             {
                 stage = 0;
-                solidMesh = false;
                 Debug.LogError("fixed stage -> now 0");
             }
         }
@@ -50,42 +89,17 @@ public class QuadSpawner : MonoBehaviour
             Debug.LogError("awaiting input");
         }
     }
-
+    private void spawnObjectTest(Vector3 point)
+    {
+        GameObject a = Instantiate(spawnTest) as GameObject;
+        a.transform.position = point;
+    }
 
 
 
     void Update()
     {
-        Debug.LogError("idle");
-        getpostitions();
-
-        if (solidMesh == true)
-        {
-            Mesh mesh = new Mesh();
-
-            Vector3[] verticies = new Vector3[3];
-            Vector2[] uv = new Vector2[3];
-            int[] triangles = new int[3];
-
-            verticies[0] = pointA;
-            verticies[1] = pointB;
-            verticies[2] = pointC;
-
-            uv[0] = new Vector2(0, 0);
-            uv[0] = new Vector2(0, 1);
-            uv[0] = new Vector2(1, 1);
-
-            triangles[0] = 0;
-            triangles[1] = 1;
-            triangles[2] = 2;
-
-            mesh.vertices = verticies;
-            mesh.uv = uv;
-            mesh.triangles = triangles;
-
-            GetComponent<MeshFilter>().mesh = mesh;
-            solidMesh = false;
-            Debug.LogError("SPAWNED");
-        }
+            createQuad();
+        
     }
 }
